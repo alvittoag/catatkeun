ceffef392cd9cc49d0a9025f32e6c6431274751d
Drian-key <riskidrian1@gmail.com>
2022-11-03 07:32:33 +0700

    fix transition on mobile first

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -7,11 +7,11 @@ import { Link } from "react-router-dom";
 export const Sidebar = ({ toggleSidebar }) => {
   return (
     <div className="mt-1 w-full p-4">
-      <h1 className="text-xs font-medium py-1 text-[#2A6DB0]">MENU</h1>
+      <h1 className="flex text-xs font-medium py-1 text-[#2A6DB0] justify-center">MENU</h1>
       <div className="flex py-2 flex-col gap-3">
         {/* Menu 1 */}
         <Link to={"/"}>
-          <div className="flex items-center py-2 gap-4 group hover:bg-gray-300 hover:rounded-xl hover:px-3 w-full hover:cursor-pointer">
+          <div className="flex items-center py-2 gap-4 group hover:bg-gray-300 rounded-xl px-3 w-full hover:cursor-pointer transition duration-500 ease-in-out">
             <div className="bg-[#EAEEF5] hover:bg-white px-2 py-1">
               <AiFillHome size={17} />
             </div>
@@ -21,8 +21,8 @@ export const Sidebar = ({ toggleSidebar }) => {
 
         {/* Menu 2 */}
         <Link to={"/todolist"}>
-          <div className="flex items-center py-2 gap-4 group hover:bg-gray-300 hover:rounded-xl hover:px-3 w-full hover:cursor-pointer">
-            <div className="bg-[#EAEEF5] px-2 py-1">
+          <div className="flex items-center py-2 gap-4 group hover:bg-gray-300 rounded-xl px-3 w-full hover:cursor-pointer transition duration-500 ease-in-out">
+            <div className="bg-[#EAEEF5] hover:bg-white px-2 py-1">
               <RiTodoFill size={17} />
             </div>
             {toggleSidebar ? null : <p>Todo List</p>}
@@ -31,8 +31,8 @@ export const Sidebar = ({ toggleSidebar }) => {
 
         {/* Menu 3 */}
         <Link to={"/schedule"}>
-          <div className="flex items-center py-2 gap-4 group hover:bg-gray-300 hover:rounded-xl hover:px-3 w-full hover:cursor-pointer">
-            <div className="bg-[#EAEEF5] px-2 py-1">
+          <div className="flex items-center py-2 gap-4 group hover:bg-gray-300 rounded-xl px-3 w-full hover:cursor-pointer transition duration-500 ease-in-out">
+            <div className="bg-[#EAEEF5] hover:bg-white px-2 py-1">
               <AiFillSchedule size={17} />
             </div>
             {toggleSidebar ? null : <p>Schedule Manager</p>}
@@ -41,8 +41,8 @@ export const Sidebar = ({ toggleSidebar }) => {
 
         {/* Menu 4 */}
         <Link to={"/chat"}>
-          <div className="flex items-center py-2 gap-4 group hover:bg-gray-300 hover:rounded-xl hover:px-3 w-full hover:cursor-pointer">
-            <div className="bg-[#EAEEF5] px-2 py-1">
+          <div className="flex items-center py-2 gap-4 group hover:bg-gray-300 rounded-xl px-3 w-full hover:cursor-pointer transition duration-500 ease-in-out">
+            <div className="bg-[#EAEEF5] hover:bg-white px-2 py-1">
               <BsFillChatSquareTextFill size={16} />
             </div>
             {toggleSidebar ? null : <p>Chat</p>}
@@ -51,8 +51,8 @@ export const Sidebar = ({ toggleSidebar }) => {
 
         {/* Menu 5 */}
         <Link to={"/group"}>
-          <div className="flex items-center py-2 gap-4 group hover:bg-gray-300 hover:rounded-xl hover:px-3 w-full hover:cursor-pointer">
-            <div className="bg-[#EAEEF5] px-2 py-1">
+          <div className="flex items-center py-2 gap-4 group hover:bg-gray-300 rounded-xl px-3 w-full hover:cursor-pointer transition duration-500 ease-in-out">
+            <div className="bg-[#EAEEF5] hover:bg-white px-2 py-1">
               <MdGroup size={17} />
             </div>
             {toggleSidebar ? null : <p>Group</p>}
