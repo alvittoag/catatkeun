ac2f31a12a9b39c6fa0834b6153b7ea6c8467eb1
Drian-key <riskidrian1@gmail.com>
2022-11-06 12:54:58 +0700

    pindah side bar ke bawah + invisible navigasi semantara

--- src/App.js
+++ src/App.js
@@ -11,24 +11,12 @@ function App() {
   return (
     <div className="flex">
       {/* Sidebar */}
-      <div
-        className={
-          toggleSidebar
-            ? "fixed w-[18%] md:w-[8%] lg:w-[5%] bg-[#FEFEFF] h-full shadow-xl"
-            : "fixed w-[45%] md:w-[28%] lg:w-[18%] bg-[#FEFEFF] h-full  shadow-xl "
-        }
-      >
+      <div className="bg-blue-500 fixed bottom-0 w-full">
         <Sidebar toggleSidebar={toggleSidebar} />
       </div>
 
       {/* Navbar and Pages */}
-      <div
-        className={
-          toggleSidebar
-            ? "absolute right-0 w-[82%] md:w-[92%] lg:w-[95%] flex-col "
-            : "absolute right-0 w-[55%] md:w-[72%] lg:w-[82%] flex-col "
-        }
-      >
+      <div className="bg-red-500 invisible">
         <Navbar handleToggleSidebar={handleToggleSidebar} />
         <div className="bg-gray-100 h-screen">
           <Root />
